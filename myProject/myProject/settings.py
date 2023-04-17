9eb3d2c6d6e8e3cd7e48b31e40ac65c634a483d3
rasilaratnakaran <rasilaratnakaran@gmail.com>
2023-04-17 14:14:34 +0000

    Home page updated

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'myProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'myapp',
+    'HOST': '127.0.0.1',
+    'PORT': '3306',
+    'USER': 'root',
+    'PASSWORD': '12345',
+
     }
 }
 
