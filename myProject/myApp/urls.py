13144d5d3d430236ee22b4137386c05b4a0413f7
rasilaratnakaran <rasilaratnakaran@gmail.com>
2023-04-19 09:08:27 +0000

    homepage updated

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -4,5 +4,4 @@ urlpatterns =[
 path('', views.home, name='myApp-home'),
 path('about/', views.about, name='myApp-about'),
 path('contact/', views.contact, name='myApp-contact'),
-path('rg/', views.rg, name='myApp-rg'),
 ]
