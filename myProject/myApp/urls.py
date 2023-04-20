2f8a19eaa58f101fa9e80b629fad8769182042a8
Ubuntu <c2035925@c2035925001.54u3mwsnyhyehlbvjgoqlz0zld.fx.internal.cloudapp.net>
2023-04-20 16:14:03 +0000

    about added

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -4,4 +4,9 @@ urlpatterns =[
 path('', views.home, name='myApp-home'),
 path('about/', views.about, name='myApp-about'),
 path('contact/', views.contact, name='myApp-contact'),
+path('reg/', views.reg, name='myApp-reg'),
+path('profile/', views.profile, name='myApp-profile'),
+path('login/', views.login, name='myApp-login'),
+path('review/', views.review, name='myApp-review'),
+path('password/', views.password, name='myApp-password'),
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -6,7 +6,20 @@ def about(request):
     return render(request, 'myApp/about.html')
 def contact(request):
     return render(request, 'myApp/contact.html')
+<<<<<<< HEAD
 
 
 
+=======
+def reg(request):
+    return render(request, 'myApp/reg.html')
+def profile(request):
+    return render(request, 'myApp/profile.html')
+def login(request):
+    return render(request, 'myApp/login.html')
+def review(request):
+    return render(request, 'myApp/review.html')
+def password(request):
+    return render(request, 'myApp/password.html')
+>>>>>>> b30e92c (about added)
 # Create your views here.
